f76535999a92d17988ca5c52eb67055cbaaf9692
Simon-Klenk <141344833+Simon-Klenk@users.noreply.github.com>
2025-02-17 15:57:27 +0100

    Add route "live" to urls in Django

--- backend/Kinderabholsystem/messages_app/urls.py
+++ backend/Kinderabholsystem/messages_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MessageListCreateAPIView, MessageStatusUpdateAPIView, ClearLayerAPIView
+from .views import MessageListCreateAPIView, MessageStatusUpdateAPIView, ClearLayerAPIView, RaspberryLiveAPIView
 
 urlpatterns = [
     path('messages/', MessageListCreateAPIView.as_view(), name='message_list_create'),
     path('messages/<int:pk>/', MessageStatusUpdateAPIView.as_view(), name='message_status_update'),
     path('clear/', ClearLayerAPIView.as_view(), name='clear_layer'),
+    path('live/', RaspberryLiveAPIView.as_view(), name='live'),
 ]
 
